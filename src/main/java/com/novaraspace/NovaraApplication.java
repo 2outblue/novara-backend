1a5af11da56a377c0df04b89814e289fb64e7f5c
2outblue <affngfrt@gmail.com>
2025-09-16 11:54:23 +0300

    Initial commit.

---
+++ src/main/java/com/novaraspace/NovaraApplication.java
@@ -0,0 +1,13 @@
+package com.novaraspace;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NovaraApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NovaraApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/novaraspace/NovaraApplicationTests.java
@@ -0,0 +1,13 @@
+package com.novaraspace;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NovaraApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
