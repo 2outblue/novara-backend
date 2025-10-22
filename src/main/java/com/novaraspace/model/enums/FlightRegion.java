e292814343193a0d367318e72807a3de76910755
2outblue <affngfrt@gmail.com>
2025-10-22 13:32:06 +0300

    Add FlightRegion enum
    Introduces the FlightRegion enum.

---
+++ src/main/java/com/novaraspace/model/enums/FlightRegion.java
@@ -0,0 +1,31 @@
+package com.novaraspace.model.enums;
+
+public enum FlightRegion {
+    EARTH,
+    NEAR_EARTH,
+    MOON,
+    MOON_ORBIT,
+    MARS,
+    MARS_ORBIT,
+    VENUS_ORBIT;
+
+    public enum EARTH_LOCATION {
+
+        KENNEDY,
+        HEATHROW,
+        NARITA,
+        BEIJING_CAPITAL,
+        DUBAI_INTL,
+        SAO_PAULO_GUARULHOS,
+        MUMBAI_CHHATRAPATI,
+        SCHIPHOL,
+        CHARLES_DE_GAULLE,
+        FRANKFURT,
+        INCHEON,
+        SYDNEY_KINGSFORD,
+        TORONTO_PEARSON,
+        SINGAPORE_CHANGI,
+        CAPE_TOWN,
+
+    }
+}
